4536fe4358dcf9146074ae78b3fbd653382b5116
sebmella <sebastien.mella@gmail.com>
2016-01-07 17:28:50 +0100

    Update step2BackgroundCorrBy3Methods.R

--- step2BackgroundCorrBy3Methods.R
+++ step2BackgroundCorrBy3Methods.R
@@ -1,9 +1,9 @@
 threeBackgroundCorrection <- function(EList, offset=18){
 	meth <- c("none", "normexp")
 	l1 <- getListOfBGCorrByMultipleMethods(data=EList, methods=meth)
-	l2 <- getListOfBGCorrByMultipleMethods(data=EList, methods="normexp", offset=eval(18))
+	l2 <- getListOfBGCorrByMultipleMethods(data=EList, methods="normexp", offset=eval(offset))
 	l3 <- append(l1,l2)
-	return(l3)
+	
 	
 	
 	#plotting the density curve for the three correction (x/y axis range = -40,150)
@@ -13,4 +13,6 @@ threeBackgroundCorrection <- function(EList, offset=18){
 	df3 <- rbind(df1,df2)
 	p <- ggplot(df3, aes(x=values, colour=as.factor(methods)))+geom_line(stat="density")+xlim(c(-40, 150))+theme_minimal()+scale_colour_discrete(name="Methods")
 	print (p)
+	
+	return(l3)
 }
